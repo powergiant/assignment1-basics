8b208961b30f9e9d170d5a183c38729c392e17b8
powergiant <xiaom@princeton.edu>
2026-04-24 21:18:17 -0400

    bpe first version

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -1,4 +1,7 @@
-def get_tokens(word: str, vocab_hash: dict):
+@profile
+def get_tokens(word: str, vocab_hash: dict) -> bytes:
+
+    word = bytes(word.encode('utf-8'))
 
     tokens = []
 
@@ -7,8 +10,9 @@ def get_tokens(word: str, vocab_hash: dict):
         i = id
         d = vocab_hash
         while i < len(word):
-            if word[i] in d:
-                d = d[word[i]]
+            b_i = bytes([word[i]])
+            if b_i in d:
+                d = d[b_i]
                 i += 1
             else:
                 break
@@ -25,7 +29,7 @@ def vocab_hash_to_list(vocab_hash: dict) -> list:
         return []
     
     for key in vocab_hash.keys():
-        l_temp = [""] + vocab_hash_to_list(vocab_hash[key])
+        l_temp = [b""] + vocab_hash_to_list(vocab_hash[key])
         l_temp = [key + v for v in l_temp]
         l += l_temp
             
@@ -34,12 +38,14 @@ def vocab_hash_to_list(vocab_hash: dict) -> list:
 def vocab_hash_add(vocab_hash: dict, v: str) -> None:
     d = vocab_hash
     for id in range(len(v)):
-        if v[id] in d:
-            d = d[v[id]]
+        b_id = bytes([v[id]])
+        if b_id in d:
+            d = d[b_id]
         else:
             for id_1 in range(id, len(v)):
-                d[v[id_1]] = {}
-                d = d[v[id_1]]
+                b_id_1 = bytes([v[id_1]])
+                d[b_id_1] = {}
+                d = d[b_id_1]
             break
 
 def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
@@ -66,19 +72,20 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tu
         except:
             counts[word.group()] = 1
 
-    vocab_ash = {}
-    for word in counts.keys():
-        word: str
-        for c in word:
-            vocab_ash[c] = {}
+    vocab_hash = {bytes([i]): {} for i in range(256)}
+    # for word in counts.keys():
+    #     word: str
+    #     for c in word.encode('utf-8'):
+    #         vocab_hash[bytes([c])] = {}
+    vocab = [(key, key[0]) for key in vocab_hash.keys()]
 
     merges = []
-    vocab_len = len(vocab_hash_to_list(vocab_ash))
+    vocab_len = len(vocab)
     while vocab_len < vocab_size:
         merge_counts = {}
         for word in counts.keys():
             count = counts[word]
-            tokens = get_tokens(word, vocab_ash)
+            tokens = get_tokens(word, vocab_hash)
             for i in range(len(tokens)-1):
                 try:
                     merge_counts[(tokens[i], tokens[i+1])] += count
@@ -88,41 +95,46 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tu
 
         merges.append(p_m)
 
-        vocab_hash_add(vocab_ash, ''.join(p_m))
+        vocab_hash_add(vocab_hash, p_m[0]+p_m[1])
+        vocab.append((p_m[0]+p_m[1], vocab_len))
         vocab_len += 1
-        assert vocab_len == len(vocab_hash_to_list(vocab_ash))
 
-    return vocab_ash, merges
+    return vocab, merges
 
 if __name__ == '__main__':
     word = "based"
 
-    vocab = {'a': {'s': {}}, 'b': {}, 'd': {}, 'e': {}, 's': {}}
+    vocab_hash = {b'a': {b's': {}}, b'b': {}, b'd': {}, b'e': {}, b's': {}}
 
-    assert get_tokens(word, vocab) == ['b', 'as', 'e', 'd']
+    assert get_tokens(word, vocab_hash) == [b'b', b'as', b'e', b'd']
 
-    assert vocab_hash_to_list(vocab) == ['a', 'as', 'b', 'd', 'e', 's']
+    assert vocab_hash_to_list(vocab_hash) == [b'a', b'as', b'b', b'd', b'e', b's']
 
-    vocab_hash_add(vocab, 'attt')
+    vocab_hash_add(vocab_hash, b'attt')
 
-    assert vocab == {'a': {'s': {}, 't': {'t': {'t': {}}}}, 'b': {}, 'd': {}, 'e': {}, 's': {}}
+    assert vocab_hash == {b'a': {b's': {}, b't': {b't': {b't': {}}}}, b'b': {}, b'd': {}, b'e': {}, b's': {}}
 
-    word = "based"
+    # word = "based"
 
-    vocab = {'a': {'s': {}}, 'b': {'a': {}}, 'd': {}, 'e': {}, 's': {}}
+    # vocab = {'a': {'s': {}}, 'b': {'a': {}}, 'd': {}, 'e': {}, 's': {}}
 
-    assert get_tokens(word, vocab) == ['ba', 's', 'e', 'd']
+    # assert get_tokens(word, vocab) == ['ba', 's', 'e', 'd']
 
     import os
     import pathlib
     import regex as re
+    import time
 
     input_path, vocab_size, special_tokens = ((pathlib.Path(os.getcwd()).resolve()) / "tests" / "fixtures" / "corpus.en",
-                                              120, '<|endoftext|>')
+                                              500, '<|endoftext|>')
     
     # input_path, vocab_size, special_tokens = ((pathlib.Path(__file__).resolve().parent) / "fixtures",
     #                                           100, '<|endoftext|>')
 
-    assert vocab_hash_to_list(train_bpe(input_path, vocab_size, special_tokens)[0]) == ['i', 'in', 'is', 'r', 're', 'o', 'or', 'n', 'nd', ' ', ' t', ' th', ' the', ' a', ' o', ' ,', ' s', ' .', ' w', ' c', 'c', 'e', 'er', 'm', 't', 's', 'a', 'at', 'd', 'y', 'f', 'u', 'p', 'w', 'h', 'he', 'l', 'b', 'k', 'g', '(', ')', '.', '\n', ',', 'v', 'P', 'O', 'B', 'x', 'N', 'T', 'A', 'L', 'W', 'E', 'D', 'I', 'M', 'S', '!', '?', 'V', '5', '#', '@', '-', 'G', '0', '8', '1', '3', '2', '4', '9', 'F', '&', ';', 'U', 'q', 'C', '6', '/', '7', 'Q', 'X', '®', 'H', 'Y', '\xad', 'j', '*', ':', '©', 'à', 'z', 'J', 'K', 'R', 'ü', 'í', 'á', 'Z', '_', '$', '%', 'ñ', '\x93', '\x94', '=', 'õ', 'ä', '€', 'ö', 'å', '\x97', '™', 'ß', '�', '+']
+    start_time = time.time()
+    assert train_bpe(input_path, vocab_size, special_tokens)[0][272:292] == [(b' c', 272), (b'on', 273), (b' b', 274), (b' f', 275), (b'en', 276), (b'ou', 277), (b'it', 278), (b'es', 279), (b' of', 280), (b' p', 281), (b'ed', 282), (b'ing', 283), (b' in', 284), (b'al', 285), (b' m', 286), (b' and', 287), (b' d', 288), (b'an', 289), (b'ar', 290), (b' to', 291)]
+    end_time = time.time()
+    print(end_time - start_time)
+    assert end_time - start_time < 1.5
     
 
